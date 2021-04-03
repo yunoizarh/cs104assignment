33e517147b53eb66fc4cfd617db97cbf183bc4a7
yunoizarh <81684797+yunoizarh@users.noreply.github.com>
2021-04-03 20:26:05 +0100

    Add files via upload

---
+++ assignment solution.py
@@ -0,0 +1,47 @@
+#a program to dislay my name and department
+name="Zubair maimunah unoiza"
+department="computer science"
+print("my name is ",name,", I am in ", department, " department")
+
+
+
+
+#a program to display boo! 
+scare="Boo!"
+print(scare)
+
+
+
+#addition of variables
+age=20
+height=6.5
+result=age+height
+print("age = ",age,"\nheight= " , height ,"\nresult=" ,result )
+
+
+#division of variables
+a=12
+b=0.5
+result=a/b
+print(int(result))
+
+
+#addition of variables
+oranges=13+9
+result=22
+print(result)
+
+
+#use of modulus
+students=8
+pencils=3
+result=8%3
+print(result)
+
+
+#use of bodmas
+a=2*4
+b=4+2
+result=a*b
+print(result)
+
